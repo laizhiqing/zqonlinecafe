4459c0b5d49aa6b229afe48ea180f4b4ceb6c13b
laizhiqing <m-10365112@moe-dl.edu.my>
2024-12-17 23:06:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+﻿const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    //Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+//Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click ());
+
+var swiper = new Swiper(".review-slider",{
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    breakpoints: {
+        0: {
+            slidesPerview: 1,
+        },
+        640: {
+            slidesPerview: 2,
+        },
+        768: {
+            slidesPerview: 2,
+        },
+        1024: {
+            slidesPerview: 3,
+        },
+    },
+});
